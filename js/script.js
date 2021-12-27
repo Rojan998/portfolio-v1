b17736a752412e436a0fda86eba23ec1c008561c
Rojan Adhikari <cmpbk55555@gmail.com>
2021-12-27 23:18:49 +0545

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,2 @@
 // For the Typing Effect
-
-const designation = ["Engineer", "Software Developer", "Designer"];
-
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-
-(function type() {
-  if (count === designation.length) {
-    count = 0;
-  }
-  currentText = designation[count];
-  letter = currentText.slice(0, ++index);
-
-  document.querySelector(".typing").textContent = letter;
-  if (letter.length === currentText.length) {
-    count++;
-    index = 0;
-  }
-  setTimeout(type, 400);
-})();
+// Silence is Golden
