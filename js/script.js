d0eca6ce0ee78416264b38fd2188df95f9d62f3e
Rojan Adhikari <adhikari.rojan619@gmail.com>
2025-03-20 19:51:05 -0500

    Made changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Silence is golden
 const startDate = () => {
   let d = new Date();
-  document.getElementById("date").innerHTML = d.getFullYear();
+  document.getElementById('date').innerHTML = d.getFullYear();
 };
 startDate();
